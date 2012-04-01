9b8b94b23254809f33a24cbaed002bc6f81462c5
Paul Baker <son.of.lysander@gmail.com>
2012-04-01 08:04:41 -0600

    Uploaded functional Greasemonkey script. Functions correctly

--- tossCenturyLinkRedirect.user.js
+++ tossCenturyLinkRedirect.user.js
@@ -11,39 +11,40 @@
 // @updated         2012-4-1
 // @compatible      Greasemonkey
 // ==/UserScript==
- 
-/* 
- * Creative Commons Attribution License (--> or Public Domain)
- * http://creativecommons.org/licenses/by/2.5/
-*/
- 
-(function(){
- 
-    //object constructor
-    function example(){
- 
-        // modify the stylesheet
-        this.append_stylesheet('body,div { border: 1px solid red; }');
- 
-    };
- 
-    //create a stylesheet
-    example.prototype.append_stylesheet = function(css){
- 
-        var styletag = document.createElement("style");
-        styletag.setAttribute('type', 'text/css');
-        styletag.setAttribute('media', 'screen');
-        styletag.appendChild(document.createTextNode(css));
- 
-        document.getElementsByTagName('head')[0].appendChild(styletag);
- 
-    };
- 
-    //instantiate and run 
-    var example = new example();
- 
- 
-})();
- 
-// you can completely copy this template, including
-// the install description, have fun! :-)
+
+//Change this variable to change your favorite search engine.
+//You can go lower down the page to add any other engine you want.
+// 1 = Google, 2 = Bing (ew), 3 = yahoo
+var pickYourSearchEngine = 1;
+//Changing this value to "false" will redirect all searches on CenturyLink done, even if they
+//where intentionally started by the user
+var dontRedirectManualSearches = false;
+
+var redirectedUrl = document.location.search;
+var theAttempt = redirectedUrl.substring(redirectedUrl.indexOf("origURL=")+8, redirectedUrl.indexOf("&r="));
+if (theAttempt.localeCompare("?search") == 0 || theAttempt.localeCompare("?queryb") == 0)
+{
+  if (dontRedirectManualSearches == true)
+  {
+    return false;
+  }
+  //I don't know why you would be doing a manual search via CenturyLink, but if you are doing this
+  //on your own then I'll let you do your thing.
+  theAttempt = redirectedUrl.substring(redirectedUrl.indexOf("querybox=")+9, redirectedUrl.indexOf("&submit"));
+}
+//now for the fun stuff.
+switch(pickYourSearchEngine)
+{
+case 1:
+  window.location = "http://www.google.com/search?as_q=" + theAttempt;
+  break;
+case 2:
+  window.location = "http://www.bing.com/search?q=" + theAttempt;
+  break;
+case 3:
+  window.location = "http://search.yahoo.com/search;_ylt=?p=" + theAttempt;
+  break;
+default:
+  window.location = "http://www.google.com/search?as_q=" + theAttempt;
+  break;
+}
